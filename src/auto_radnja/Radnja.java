d9bfbd7d6ffc0f49576dee6d3125361a64bb44af
tamaratosic98 <tosict98@gmail.com>
2021-03-18 11:15:14 +0100

    Dokumentovan interfejs Radnja.

--- src/auto_radnja/Radnja.java
+++ src/auto_radnja/Radnja.java
@@ -1,8 +1,30 @@
 package auto_radnja;
 import java.util.LinkedList;
 import auto_radnja.gume.AutoGuma;
-
+/**
+ * Intefejs koji predstavlja Radnju.
+ * 
+ * @author tamara
+ * @version 2.0
+ */
 public interface Radnja {
+	/**
+	 * Dodaje novu gumu u radnju.
+	 * @param a Auto guma koja se dodaje.
+	 * 
+	 * @throws java.lang.NullPointerException Ako je uneta guma null.
+	 * @throws java.lang.RuntimeException Ako uneta guma vec postoji u radnji (duplikat).
+	 */
 	void dodajGumu(AutoGuma a);
+	/**
+	 * Pronalazi gumu u radnji po zadatom parametru markaModel.
+	 * @param markaModel Marka i model po kome se pretrazuje radnja.
+	 * @return 
+	 * <ul>
+	 * 		<li> Null ukoliko je prosledjen markaModel null. </li>
+	 * 		<li> Lista auto guma koja odgovara kriterijumu pretrage. </li>
+	 * </ul>
+	 * 
+	 */
     LinkedList<AutoGuma> pronadjiGumu(String markaModel);
 }
